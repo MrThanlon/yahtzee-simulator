f27bdaf7a0bfad810fe11c0e3042bce3df198485
huangziyi <huangziyi@canaan-creative.com>
2025-01-14 17:41:33 +0800

    10k tests
    Signed-off-by: huangziyi <huangziyi@canaan-creative.com>

--- strategies/random.ts
+++ strategies/random.ts
@@ -1,25 +1,33 @@
 import { z } from "zod"
 import { finalScore, initialState, isGameOver, reRoll, ScoresKey, selectScore, State } from "../state.js"
 
-const state = initialState()
-
 function ramdomYesOrNo() {
   return Math.random() < 0.5
 }
 
 const scoresKey = Object.keys(State.shape.scores.shape)
 
-while (isGameOver(state) === false) {
-    while (ramdomYesOrNo()) {
-        if (!reRoll(state, [0, 1, 2, 3, 4])) {
-            // no more reroll
-            break
+let scores = 0
+const numRun = 10000
+
+for (let i = 0; i < numRun; i++) {
+    const state = initialState()
+
+    while (isGameOver(state) === false) {
+        while (ramdomYesOrNo()) {
+            if (!reRoll(state, [0, 1, 2, 3, 4])) {
+                // no more reroll
+                break
+            }
+        }
+        let index = Math.floor(Math.random() * 12)
+        while (!selectScore(state, scoresKey[index] as ScoresKey)) {
+            index = Math.floor(Math.random() * 12)
         }
     }
-    let index = Math.floor(Math.random() * 12)
-    while (!selectScore(state, scoresKey[index] as ScoresKey)) {
-        index = Math.floor(Math.random() * 12)
-    }
+
+    const score = finalScore(state)
+    scores += score
 }
 
-console.log(finalScore(state))
+console.log(`Average score: ${scores / numRun}`)
