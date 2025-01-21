f9305bebf8070cd45dd6c220c210b3ef80eabc1f
huangziyi <huangziyi@canaan-creative.com>
2025-01-21 17:29:42 +0800

    update
    Signed-off-by: huangziyi <huangziyi@canaan-creative.com>

--- web/main.ts
+++ web/main.ts
@@ -7,21 +7,32 @@ const gravity = { x: 0.0, y: -9.81, z: 0.0 }
 const world = new RAPIER.World(gravity)
 
 // Create a dynamic rigid-body.
-const rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
-    .setTranslation(0.0, 3.0, 0.0)
-    .setRotation({ x: 1.0, y: 0.3, z: 0.1, w: 1.0 })
-const rigidBody = world.createRigidBody(rigidBodyDesc)
+const rigidBody1 = world.createRigidBody(
+    RAPIER.RigidBodyDesc.dynamic()
+        .setTranslation(0.0, 3.0, 0.0)
+        .setRotation({ x: 1.0, y: 0.3, z: 0.1, w: 1.0 })
+)
+
+const rigidBody2 = world.createRigidBody(
+    RAPIER.RigidBodyDesc.dynamic()
+        .setTranslation(1.0, 3.0, 0.0)
+        .setRotation({ x: 1.0, y: 0.3, z: 0.1, w: 1.0 })
+)
 
 // Create a cuboid collider attached to the dynamic rigidBody.
-const colliderDesc = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5).setRestitution(0.7)
-const collider = world.createCollider(colliderDesc, rigidBody)
+const colliderDesc = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5)
+    .setRestitution(0.02)
+    .setFriction(50)
+    .setMass(100.0)
+const collider1 = world.createCollider(colliderDesc, rigidBody1)
+const collider2 = world.createCollider(colliderDesc, rigidBody2)
 
-const containerBody = world.createRigidBody(RAPIER.RigidBodyDesc.kinematicVelocityBased())
+const containerBody = world.createRigidBody(RAPIER.RigidBodyDesc.kinematicPositionBased())
 const containerShape = cylinderContainer(2, 5, 16)
 const container = RAPIER.ColliderDesc.trimesh(
     containerShape.vertices,
     containerShape.indices
-).setRestitution(0.7).setFriction(0)
+).setRestitution(0.02).setFriction(50)
 world.createCollider(container, containerBody)
 
 // const container = RAPIER.ColliderDesc.trimesh(new Float32Array([]), new Uint32Array([]))
@@ -32,18 +43,18 @@ document.body.style.padding = '0'
 
 const width = window.innerWidth
 const height = window.innerHeight
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( width, height );
-renderer.setPixelRatio( window.devicePixelRatio );
-document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize( width, height )
+renderer.setPixelRatio( window.devicePixelRatio )
+document.body.appendChild( renderer.domElement )
 
-const camera = new THREE.PerspectiveCamera( 70.0, width / height, 0.1, 20.0 );
-camera.position.set( 0.0, 1.0, -5.0 );
+const camera = new THREE.PerspectiveCamera( 70.0, width / height, 0.1, 20.0 )
+camera.position.set( 0.0, 1.0, -5.0 )
 
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.screenSpacePanning = true;
-controls.target.set( 0.0, 1.0, 0.0 );
-controls.update();
+const controls = new OrbitControls( camera, renderer.domElement )
+controls.screenSpacePanning = true
+controls.target.set( 0.0, 1.0, 0.0 )
+controls.update()
 
 const scene = new THREE.Scene()
 // const ambientLight = new THREE.AmbientLight(0x606060)
@@ -60,23 +71,20 @@ const lines = new THREE.LineSegments(geometry, material)
 scene.add(lines)
 
 // Game loop. Replace by your own game loop system.
+let positionX = 0
+let positionY = 0
+document.addEventListener('mousemove', e => {
+    positionX = e.clientX / width - 0.5
+    positionY = e.clientY / height - 0.5
+})
 const startTime = Date.now()
 const gameLoop = () => {
     // Step the simulation forward.  
-    if (Date.now() - startTime < 10000) {
-        const amp = 10
-        if (1) {
-            const x = (Math.random() - 0.5) * amp
-            const y = 0 //(Math.random() - 0.5) * amp
-            const z = (Math.random() - 0.5) * amp
-            containerBody.setLinvel(new RAPIER.Vector3(x, y, z), true)
-            // containerBody.setTranslation(new RAPIER.Vector3(x, y, z), true)
-        }
-        world.step()
-    }
+    containerBody.setTranslation(new RAPIER.Vector3(positionX * 2, Math.random() * 0.001, positionY * 2), true)
+    world.step()
 
     // Get and print the rigid-body's position.
-    const position = rigidBody.translation()
+    // const position = rigidBody.translation()
     // console.log("Rigid-body position: ", position.x, position.y, position.z)
 
     const buffers = world.debugRender()
